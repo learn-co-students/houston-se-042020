5de61c4cab9c878155b990bbe8ed2892c6075e02
vidhisharma3193 <vidhisharma3193@gmail.com>
2020-05-12 11:23:18 -0500

    done

--- 15-forms-restful/app/controllers/application_controller.rb
+++ 15-forms-restful/app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < Sinatra::Base
 
     set :views, "app/views"
+    set :method_override, true
 
     get '/' do
         "Hello World"
@@ -19,12 +20,44 @@ class ApplicationController < Sinatra::Base
         erb :index
     end
 
+    # new book form
+    get '/books/new' do 
+        erb :new
+    end
+
     # display single book info
     get '/books/:id' do 
         # binding.pry
         @book = Book.find(params[:id]) 
         erb :show
     end
+
+    post '/books' do 
+        # binding.pry
+        book = Book.create(params)
+        redirect "/books/#{book.id}"
+    end
+
+    get '/books/:id/edit' do
+        @book = Book.find(params[:id]) 
+        erb :edit
+    end
+
+    patch '/books/:id' do
+        # binding.pry
+        book = Book.find(params[:id])
+        book.update(title: params[:title], year: params[:year])
+        redirect "/books/#{book.id}"
+    end
+
+    delete '/books/:id' do 
+        book = Book.find(params[:id])
+        book.destroy
+        # Book.delete(params[:id])
+        redirect "/books"
+    end
+
+  
 
 
 end
