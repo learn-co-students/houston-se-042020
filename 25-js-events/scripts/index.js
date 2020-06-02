2c3c634b73caea9ada5fc60812f4e58561619df8
Rupa <rupa211984@gmail.com>
2020-06-02 11:05:07 -0700

    Add form reset

--- 25-js-events/scripts/index.js
+++ 25-js-events/scripts/index.js
@@ -20,4 +20,54 @@ form.addEventListener('submit', function (e) {
   const comment = document.querySelector('#comment').value;
 
   createPost(gifUrl, comment);
+  form.reset(); // clear the inputs in the form
 });
+
+
+
+
+
+// Someone asked how to pass a named function to addEventListener, and I don't think I answered the Q properly
+// function createPostOnSubmit() {
+//   event.preventDefault();
+
+//   const gifUrl = document.querySelector('#gif-url').value;
+//   const comment = document.querySelector('#comment').value;
+//   // make post div
+//   const div = document.createElement('div');
+//   const img = document.createElement('img');
+//   const p = document.createElement('p');
+
+//   img.setAttribute('src', gifUrl);
+//   p.textContent = comment;
+//   div.append(img, p);
+
+//   const posts = document.querySelector('.posts');
+//   posts.append(div);
+// }
+
+// form.addEventListener('submit', createPostOnSubmit);
+
+
+// And here's another way
+// function createPostOnSubmit(gifUrl, comment, e) {
+//   e.preventDefault();
+//   // make post div
+//   const div = document.createElement('div');
+//   const img = document.createElement('img');
+//   const p = document.createElement('p');
+
+//   img.setAttribute('src', gifUrl);
+//   p.textContent = comment;
+//   div.append(img, p);
+
+//   const posts = document.querySelector('.posts');
+//   posts.append(div);
+// }
+
+// form.addEventListener('submit', function () {
+//   const gifUrl = document.querySelector('#gif-url').value;
+//   const comment = document.querySelector('#comment').value;
+
+//   createPostOnSubmit(gifUrl, comment, event);
+// });
