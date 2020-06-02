af6446deb07cb4a0185536ccf0f083f8efb6640a
Rupa <rupa211984@gmail.com>
2020-06-02 10:02:45 -0700

    Houston solution

--- 25-js-events/scripts/index.js
+++ 25-js-events/scripts/index.js
@@ -1,1 +1,23 @@
-console.log(document.querySelector('form'));
+function createPost(gifUrl, comment) {
+  // make post div
+  const div = document.createElement('div');
+  const img = document.createElement('img');
+  const p = document.createElement('p');
+
+  img.setAttribute('src', gifUrl);
+  p.textContent = comment;
+  div.append(img, p);
+
+  const posts = document.querySelector('.posts');
+  posts.append(div);
+}
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const gifUrl = document.querySelector('#gif-url').value;
+  const comment = document.querySelector('#comment').value;
+
+  createPost(gifUrl, comment);
+});
