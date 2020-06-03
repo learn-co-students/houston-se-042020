952633577835a1b53c87ffe1824ce2d371529236
Rupa <rupa211984@gmail.com>
2020-06-03 10:05:45 -0700

    Clear up some issues from lecture

--- 25-js-events/scripts/index.js
+++ 25-js-events/scripts/index.js
@@ -24,7 +24,21 @@ form.addEventListener('submit', function (e) {
 });
 
 
+// I was asked early in lecture about the TypeError that occurs when passing
+// a callback that is not a function, and I don't think I explained it well
+// I hope this example helps clear things up
+function returnFive() {
+  return 5;
+}
 
+function logWhatever(cbFn) {
+  console.log(cbFn());
+}
+
+returnFive(logWhatever); // passed function by reference, no error
+returnFive(logWhatever()); // executed function, essentially passing in the integer 5
+    // Results in Uncaught TypeError, cbFn is not a function
+    // cbFn is the name of the parameter in logWhatever(), and is not a function
 
 
 // Someone asked how to pass a named function to addEventListener, and I don't think I answered the Q properly
